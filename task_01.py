78638426275bf3455a6939c2398c1efcfb6b23b3
NipunaDakshina <87487225+NipunaDakshina@users.noreply.github.com>
2024-11-18 09:27:09 +0530

    add task for getting factorial

--- task_01.py
+++ task_01.py
@@ -1,1 +1,7 @@
-print("Nipuna Dakshina")
+# this code is used for getting factorial of a number
+
+num=int(input("Enter a number : "))
+fact=1
+for i in range(1,num+1):
+    fact=fact*i
+print("Factorial of ",num," is ",fact)
